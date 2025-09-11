415a3de80d1c00f9ef6220410152760c68d0411b
sharifjonakramov9.bit <sharifjonakramov9@gemail.com>
2025-09-11 07:14:44 +0300

    practice06.py

--- practice06.py
+++ practice06.py
@@ -1,1 +1,16 @@
-# tushunmadim
+def is_valid_phone_number(phone):
+    result = phone.isdigit() and len(phone) == 9
+
+    return result
+
+tries = 0
+while True:
+    phone = input("Phone: ")
+    tries += 1
+    if is_valid_phone_number(phone):
+        print(f"Phone number to'g'ri: {tries}")
+        break
+    else:
+        print("Phone number noto'g'ri")
+        continue
+
